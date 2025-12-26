ebe33c3978b1c342f5f1077ad04a1741a7c4f873
Badal Mehra <badalmehra142@gmail.com>
2025-12-27 02:12:29 +0530

    Add temporary button to force enable push notifications

--- src/pages/MyCart.tsx
+++ src/pages/MyCart.tsx
@@ -156,7 +156,11 @@ const MyCart = () => {
           </div>
         </div>
       </header>
-
+{/* Temp */}
+      <button onClick={() => subscribeToPush(user.id)}>
+  FORCE ENABLE PUSH
+</button>
+{/* Temp */}
       <div className="container mx-auto px-4 py-6 max-w-6xl">
         {favorites.length === 0 ? (
           <div className="text-center py-16">
